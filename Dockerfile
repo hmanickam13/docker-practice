4c5e379db6bbeda352d37e51cc008ed7a50499bf
hmanickam13 <hmanickam13@gmail.com>
2023-06-01 21:12:57 -0500

    new files

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,19 @@
-FROM node:alpine
-COPY . /app
-# CMD node /app/app.js
+# Layers get cached if nothing changes
+FROM node:12
+
 WORKDIR /app
-CMD node app.js
+
+COPY package*.json ./
+
+RUN npm install
+# (SHELL FORM)
+
+COPY . .
+
+ENV PORT=8080
+
+EXPOSE 8080
+
+# Only 1 of these per dockerfile
+CMD [ "npm", "start"]
+# (EXEC FORM) - preferred way as it doesnt start up a shell session
--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello Docker!");
+// Import the Express module and create an instance of the Express application
+const app = require('express')();
+
+// Define a route for the root URL ('/') that handles GET requests
+app.get('/', (req, res) => {
+    // Send a JSON response with a message
+    res.json({ message: 'Docker is easy' });
+});
+
+// Define the port to listen on, using either the value from the environment variable PORT or 8080 as a fallback
+const port = process.env.PORT || 8080;
+
+// Start the server and listen on the specified port
+app.listen(port, () => console.log(`app listening on http://localhost:${port}`));
